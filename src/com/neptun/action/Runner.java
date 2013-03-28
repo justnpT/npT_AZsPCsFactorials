1bbdaa9fcd8720acdae900c2b31f3cacd691c22e
Michal NeptuN Nowierski <Michal.Nowierski@gmail.com>
2013-03-28 23:39:08 +0100

    main change

--- src/com/neptun/action/Runner.java
+++ src/com/neptun/action/Runner.java
@@ -17,77 +17,7 @@ public class Runner {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
-		try {
-		System.out.println(decimalSilnia(new BigDecimal(13)));
-
-		SLPlog log = new SLPlog();
-		BigDecimal target = decimalSilnia(new BigDecimal(7));
-		int[] forInputArray = {2,3,5};
-		SLPinputList inputList = new SLPinputList(forInputArray, target);
-
-		ArrayList<SLPresult> finalResults = new ArrayList<SLPresult>();
-		ArrayList<char[]> equationMatrix = prepareEquationMatrix(3);
-//		ArrayList<char[]> equationMatrix = new ArrayList<char[]>();
-//		char[] test = {'v','+','v','-','v','*','v'};
-//		equationMatrix.add(test);
-		
-	for (int t = 0; t < equationMatrix.size(); t++) {
-		System.out.println("analizuje rownanie numer "+(t+1)+". Rownan: "+equationMatrix.size());
-		
-		SLPclock clock = new SLPclock(equationMatrix.get(t), inputList.size());
-		SLPequiation slpEquation = new SLPequiation(equationMatrix.get(t), inputList, clock);
-
-		BigDecimal score = slpEquation.nextIteration();
-		SLPresult currentResult = new SLPresult(target, score);
-
-		SLPresult nextResult= new SLPresult(target, score);
-
-//		log.logResult(currentResult);
-		while (slpEquation.hasNext()) {
-			score = slpEquation.nextIteration();
-			nextResult = new SLPresult(target, score);
-			if (nextResult.compareTo(currentResult)==1) {
-				currentResult = nextResult;		
-				currentResult.setEquation(slpEquation.getPreviousEquation());
-//				log.logResult(currentResult);
-//				log.logEquation(currentResult.getEquation());
-			}
-			
-		}
-		finalResults.add(currentResult);
-		System.out.println("rezultat rownania: "+currentResult.getScore());
-		System.out.println("rownanie: "+currentResult.getEquation());
-		System.out.println("odleglos od targetu: "+currentResult.getDistance());
-		System.out.println("--------------------------------------------------");
-	}
-	
-	System.out.println("koniec poszukiwan");
-	
-	SLPresult rezultat = finalResults.get(0);
-	for (int i = 0; i < finalResults.size(); i++) {
-//		System.out.println("rezultat finalowy numer "+i);
-//		log.logResult(finalResults.get(i));
-//		log.logEquation(finalResults.get(i).getEquation());
-		if (finalResults.get(i).compareTo(rezultat)==1) {
-			rezultat = finalResults.get(i);
-			System.out.println("lepszy rezultat: "+rezultat.getScore());
-			System.out.println("jego rownanie: "+rezultat.getEquation());
-		}
-
-	}
-	System.out.println("==========================");
-	System.out.println(" koniec, target to: "+target);
-	System.out.println("najlepszy wynik to: "+rezultat.getScore());
-	System.out.println("rownianie to: "+rezultat.getEquation());
-
-	}
-		catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}				
-	}
+	public static void main(String[] args) {}
 
 	/**
 	 * prepare equation with set number of operations
