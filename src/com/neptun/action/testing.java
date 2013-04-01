3ef095cd2bc09d8221c18267b83b8f1068cfe8de
Michal NeptuN Nowierski <Michal.Nowierski@gmail.com>
2013-04-01 13:15:46 +0200

    ready

--- src/com/neptun/action/testing.java
+++ src/com/neptun/action/testing.java
@@ -1,6 +1,5 @@
 package com.neptun.action;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class testing {
@@ -8,27 +7,9 @@ public class testing {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		LinkedList<char[]> items = new LinkedList<char[]>();
-        char[] item = new char[13];
-        char[] input = {'+', '-', '*'};
-        rep(items, input, item, 0);
-
-        ArrayList<char[]> matrix = new ArrayList<char[]>();
-        
-        int size = (items.get(0).length*2)+1;
-        char[] equation = new char[size];
-        for (char[] rep : items) {
-        	
-        	equation[0] = 'v';
-        	for (int i = 0; i < rep.length; i++) {
-				equation[1+2*i] = rep[i];
-				equation[2*(1+i)] = 'v';
-			}
-            System.out.println(rep);
-            System.out.println(equation);
-        }
-	}
+//	public static void main(String[] args) {
+//		System.out.println("siemanko");
+//	}
 	private static void rep(LinkedList<char[]> reps, char[] input, char[] item, int count){
         if (count < item.length){
             for (int i = 0; i < input.length; i++) {
